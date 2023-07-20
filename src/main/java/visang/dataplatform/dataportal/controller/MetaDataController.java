844b253a1347f2a208751c533f581098e152450d
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-07-20 17:11:55 +0900

    fix: controller, service, mapper 추가
    데이터 맵, 데이터 기반 조직도, 메타데이터 정보 메뉴에 맞게 필요한 controller, service, mapper 추가

---
+++ src/main/java/visang/dataplatform/dataportal/controller/MetaDataController.java
@@ -0,0 +1,15 @@
+package visang.dataplatform.dataportal.controller;
+
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("apis/meta")
+@Api(tags = { "MetaDataInfo API" }, description = "메타데이터 정보 API")
+public class MetaDataController {
+}
---
+++ src/main/java/visang/dataplatform/dataportal/mapper/MetaDataMapper.java
@@ -0,0 +1,7 @@
+package visang.dataplatform.dataportal.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MetaDataMapper {
+}
