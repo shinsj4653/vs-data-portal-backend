00c214a0aa06e1064d6d988e16c446413ca7b9b0
김도원 <kdwon7592@naver.com>
2023-07-25 11:34:14 +0900

    Feat: 개발 서버 cors 허용

--- src/main/java/visang/dataplatform/dataportal/WebConfig.java
+++ src/main/java/visang/dataplatform/dataportal/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") //로컬
-                .allowedOrigins("http://3.34.194.218:3000") //개발
+                .allowedOrigins("http://localhost:3000", "http://3.34.194.218:3000") //로컬, 개발
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 쿠키 허용
                 .maxAge(3600); // preflight(사전 요청)의 캐시 시간 설정
