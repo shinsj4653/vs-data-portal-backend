76da253b65ce1ac03941b6e6a8fb14087369971e
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-08-10 15:17:22 +0900

    fix: 메인 화면에서 검색 시, 서비스 명 혹은 데이터 셋 명이 null 인 요소는 조회 안되도록 수정

--- src/main/java/visang/dataplatform/dataportal/model/dto/dpmain/DatasetSearchDto.java
+++ src/main/java/visang/dataplatform/dataportal/model/dto/dpmain/DatasetSearchDto.java
@@ -3,8 +3,6 @@ package visang.dataplatform.dataportal.model.dto.dpmain;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.ibatis.type.Alias;
-
 @Getter
 @AllArgsConstructor
 @Schema(description = "데이터 포털 메인 - 데이터 셋 검색 결과 DTO")
