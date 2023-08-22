9093e4303eb30f07e3720e292a82c6f698354758
SEONG JUN SHIN <shinsj4653@konkuk.ac.kr>
2023-08-22 17:13:06 +0900

    feat: 메타 테이블 컬럼 정보 반환 Query 응답 Dto 생성

---
+++ src/main/java/visang/dataplatform/dataportal/model/query/metadata/QueryResponseTableColumnInfo.java
@@ -0,0 +1,11 @@
+package visang.dataplatform.dataportal.model.query.metadata;
+
+import lombok.Getter;
+
+@Getter
+public class QueryResponseTableColumnInfo {
+    private String table_col_id;
+    private String table_col_name;
+    private String table_col_datatype;
+    private String table_col_comment;
+}
